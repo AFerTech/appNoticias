314f3de0dca90e2a32552abe31223ff9566fcb87
Alexis Fernando <alexis96fer@gmail.com>
2022-03-30 15:01:23 -0600

    modificación de los componente article/articles

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../interfaces';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+  @Input() index:number;
 
   constructor() { }
 
